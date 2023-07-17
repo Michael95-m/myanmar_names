5aecce250ec157572a5aeea6a2f97eb489b1739b
Michael95-m <minkhantmgmg.mk19@gmail.com>
2023-07-17 13:33:14 +0630

    change some function for better code quality and write docstring

--- mm_names/syl.py
+++ mm_names/syl.py
@@ -1,17 +1,40 @@
 ## This syllable segmentation code is from sayar ye kyaw thu's github
 ## I changed a little bit
 
+"""
+Module Name: syl Module
+Description: This module segments a string in Burmese into syllables.
+"""
+
 import re
-   
-myConsonant = r"က-အ"
-enChar = r"a-zA-Z0-9"
-otherChar = r"ဣဤဥဦဧဩဪဿ၌၍၏၀-၉၊။!-/:-@[-`{-~\s"
-ssSymbol = r'္'
-aThat = r'်'
 
 
-BreakPattern = re.compile(r"((?<!" + ssSymbol + r")["+ myConsonant + r"](?![" + aThat + ssSymbol + r"])" + r"|[" + enChar + otherChar + r"])", re.UNICODE)
+class SylBreak:
+    # pylint: disable=too-few-public-methods
+    """
+    A class for segmentation of a string in burmese into syllables
+    """
 
+    def __init__(self):
+        self.my_consonant = r"က-အ"
+        self.en_char = r"a-zA-Z0-9"
+        self.other_char = r"ဣဤဥဦဧဩဪဿ၌၍၏၀-၉၊။!-/:-@[-`{-~\s"
+        self.ss_symbol = r"္"
+        self.a_thet = r"်"
+        self.break_pattern = re.compile(
+            rf"((?<!{self.ss_symbol})[{self.my_consonant}](?![{self.a_thet}{self.ss_symbol}])|"
+            "[{self.en_char}{self.other_char}])",
+            re.UNICODE,
+        )
 
-def syllable(line: str, delimiter: str=" "):
-    return BreakPattern.sub(delimiter + r"\1", line).strip()
+    def syllable(self, line: str, delimiter: str = " ") -> str:
+        """
+        Segment a string in Burmese into syllables
+        Args:
+            line (str): a string in Burmese
+            delimiter (str, optional): string to join different syllables. Defaults to " ".
+
+        Returns:
+            str: a string in Burmese in syllable segmentation
+        """
+        return self.break_pattern.sub(delimiter + r"\1", line).strip()
