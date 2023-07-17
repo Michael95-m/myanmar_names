b37452515be58d3ba601870c59fd57cbd6c042dc
Michael95-m <minkhantmgmg.mk19@gmail.com>
2023-07-17 13:32:21 +0630

    write docstring

--- mm_names/__init__.py
+++ mm_names/__init__.py
@@ -1,7 +1,12 @@
-import json 
+"""
+Module Name: mm_names Module
+Description: This module loads JSON dictionaries from files.
+"""
 
-with open("data/en2mm.json") as f:
+import json
+
+with open("data/en2mm.json", encoding="utf-8") as f:
     en2mm_dict = json.load(f)
 
-with open("data/mm2en.json") as f:
+with open("data/mm2en.json", encoding="utf-8") as f:
     mm2en_dict = json.load(f)
